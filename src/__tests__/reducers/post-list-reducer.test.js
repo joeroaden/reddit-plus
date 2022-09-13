f59d7b6bd6cfc10186514c0260de87a6675b0b42
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-09-13 14:27:51 -0700

    add redux to project

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -1,7 +1,64 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
+
+const currentState = {
+    1:{
+    userName: 'Ryan',
+    topic: 'stuff',
+    postBody: 'Redux action is not working correctly.',
+    id: 1
+  }, 2: {
+    userName: 'Jasmine',
+    topic: 'more stuff',
+    postBody: 'Reducer has side effects.',
+    id: 1
+  }
+}
+
+  let action;
+  const postData = {
+    userName: 'Ryan',
+    topic: 'stuff',
+    postBody: 'Redux action is not working correctly.',
+    id: 1
+  };
+  
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(postListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new post data to mainPostList', () => {
+    const { userName, topic, postBody, id } = postData;
+    action = {
+      type: 'ADD_POST',
+      userName: userName,
+      topic: topic,
+      postBody: postBody,
+      id: id
+    };
+
+    expect(postListReducer({}, action)).toEqual({
+      [id] : {
+        userName: userName,
+        topic: topic,
+        postBody: postBody,
+        id: id
+      }
+    });
+  });
+    test('Should successfully delete a post', () => {
+      action = {
+        type: 'DELETE_POST',
+        id: 1
+      };
+      expect(postListReducer(currentState, action)).toEqual({
+        2: {
+          userName: 'Jasmine',
+          topic: 'more stuff',
+          postBody: 'Reducer has side effects.',
+          id: 1
+        }
+      })
+  })
 });
--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,5 +1,22 @@
 const reducer = (state = {}, action) => {
+  const { userName, topic, postBody, id } = action;
+  switch (action.type) {
+  case 'ADD_POST':
+    return Object.assign({}, state, {
+      [id]: {
+        userName: userName,
+        topic: topic,
+        postBody: postBody,
+        id: id
+      }
+    });
+  case 'DELETE_POST':
+    let newState = { ...state};
+    delete newState[id];
+    return newState;
+  default:
   return state;
+  }
 };
 
 export default reducer; 
