2a0bf6a0b5a736eca7914115ad8567e9fd57f397
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-09-13 12:00:02 -0700

    update App.js with new project structure and add new form fields to Reusable form

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='userName'
+          placeholder='Username' />
+        <input
+          type='text'
+          name='topic'
+          placeholder='Topic' />
+        <textarea
+          name='postBody'
+          placeholder='Add the body of your post here.' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
